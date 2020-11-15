4197e16812746082f6e318ed591b39b8b1cdbfa5
Tomas Perez <tomasitopg6@gmail.com>
2020-11-15 10:11:47 +0000

    added video game view files

--- app/helpers/video_games_helper.rb
+++ app/helpers/video_games_helper.rb
@@ -1,11 +1,11 @@
 module VideoGamesHelper
 
-  def genre_options(genres, video_games)
+  def genre_options(genres, video_game)
     if genres.count > 0
       render :partial => "genres/details", :locals => {:genres => genres, :video_games => video_games}
     else
       "No genres found"
     end
   end
-  
+
 end
