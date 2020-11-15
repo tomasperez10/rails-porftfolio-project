efec4b3ddcd759e916b7814249a3ac261b7d64e8
Tomas Perez <tomasitopg6@gmail.com>
2020-11-15 10:59:55 +0000

    added methods to model

--- app/models/video_games.rb
+++ app/models/video_games.rb
@@ -9,4 +9,19 @@ class VideoGames < ApplicationRecord
 
   scope :search, -> (query) { where("title LIKE ?", "%" + query + "%") }
 
+  def genres_attributes=(genre_attributes)
+    genre_name = genre_attributes["0"]["name"]
+    unless genre_name.blank?
+      genre = Genre.find_or_create_by(name: genre_name)
+      self.genres << genre
+    end
+  end
+
+  private
+  def destroy_video_games
+    self.video_games.each do |video_game|
+      video_game.destroy
+    end
+  end
+
 end
